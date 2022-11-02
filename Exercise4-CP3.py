c6a9d216e1a9d1af4713ec94ec059526e6e82fff
jaydeewannabeyours <117032013+jaydeewannabeyours@users.noreply.github.com>
2022-11-02 23:37:02 +0700

    Add files via upload

---
+++ Exercise4-CP3.py
@@ -0,0 +1,9 @@
+FoundationEnglish = 90
+GeneralBusiness = 80
+IntroComSystem = 90
+Computerprogram = 80
+print("--- Your Score ---")
+print("Foundation English :", FoundationEnglish)
+print("General Business : ", GeneralBusiness)
+print("Computer Programming : ", Computerprogram)
+print("Introduction to Computer Systems :", IntroComSystem)
